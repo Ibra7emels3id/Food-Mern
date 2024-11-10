5d818e5ef9570cae0d355e71cd3b2609b5481675
Ibrahem Elsaid <158269891+Ibra7emels3id@users.noreply.github.com>
2024-11-10 22:49:00 +0200

    first commit

--- src/pages/Home/_components/AlertOffer.jsx
+++ src/pages/Home/_components/AlertOffer.jsx
@@ -58,7 +58,7 @@ const AlertOffer = () => {
         return (
             <div className={`flex ${showAlert} fixed items-center justify-center z-[10000] left-0 top-0 w-full h-screen bg-[#000000c2]`}>
                 <div className="z-[10000]">
-                    <div className="flex flex-col relative bg-slate-200 h-[550px] w-[450px] rounded-3xl">
+                    <div className="flex flex-col  relative bg-slate-200 h-[550px] w-[350px] md:w-[450px] rounded-3xl">
                         <div
                             onClick={handleClose}
                             className="icon absolute bg-yellow rounded-ss-3xl w-[50px] h-[50px] flex items-center justify-center cursor-pointer"
@@ -73,11 +73,11 @@ const AlertOffer = () => {
                                     className="md:w-[190px] w-36"
                                 />
                             </Link>
-                            <h2 className='text-center mt-5 font-bold text-yellow text-4xl'>Discounts up to 30%</h2>
+                            <h2 className='text-center mt-5 font-bold text-yellow text-2xl md:text-4xl'>Discounts up to 30%</h2>
                             <div className="flex flex-col items-center gap-20 mt-10">
                                 {offers.slice(0, 2).map((offer, index) => (
                                     <p key={index} className='flex gap-3'>
-                                        <span className='text-3xl font-bold'>{offer.code}</span>
+                                        <span className='text-2xl md:text-3xl font-bold'>{offer.code}</span>
                                         <button onClick={() => handleCopyCode(offer.code)} className='text-yellow'>
                                             <ContentCopyIcon sx={{ marginLeft: '20px', fontSize: '30px' }} />
                                         </button>

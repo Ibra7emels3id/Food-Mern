c7f4ea5078f44d8faea9a5ebeb6d1233ed21472a
Ibrahem Elsaid <158269891+Ibra7emels3id@users.noreply.github.com>
2024-11-10 23:10:17 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -68,6 +68,21 @@ function App() {
         }
     }, [dispatch, userId, token]);
 
+
+
+    if ('serviceWorker' in navigator) {
+        window.addEventListener('load', () => {
+            navigator.serviceWorker.register('/service-worker.js').then(registration => {
+                console.log('Service Worker registered:', registration);
+            }).catch(error => {
+                console.log('Service Worker registration failed:', error);
+            });
+        });
+    }
+
+
+
+
     return (
         <>
             <BrowserRouter basename="/">

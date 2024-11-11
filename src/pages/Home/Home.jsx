8f66d99d74c51b46cc2ea2fd059b9a78129efdd4
Ibrahem Elsaid <158269891+Ibra7emels3id@users.noreply.github.com>
2024-11-11 13:29:35 +0200

    first commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,39 +3,52 @@ import Discount from './_components/Discount';
 import PopularProducts from './_components/PopularProducts';
 import JustArrived from './_components/JustArrived';
 import Loading from '../../Components/Loading';
+import { useSelector } from 'react-redux';
+
+const Header = lazy(() => import('../../Components/Header'));
+const MainSection = lazy(() => import('./_components/MainSection'));
+const Category = lazy(() => import('./_components/Category'));
+const CannedFood = lazy(() => import('./_components/CannedFood'));
+const Products = lazy(() => import('./_components/Products'));
+const Offers = lazy(() => import('./_components/Offers'));
+const SellingProducts = lazy(() => import('./_components/SellingProducts'));
+const OurBlog = lazy(() => import('./_components/OurBlog'));
+const ShopApp = lazy(() => import('./_components/ShopApp'));
+const Discover = lazy(() => import('./_components/Discover'));
+const Footer = lazy(() => import('../../Components/Footer'));
+const AlertOffer = lazy(() => import('./_components/AlertOffer'));
 
 const Home = () => {
-    const Header = lazy(() => import('../../Components/Header'));
-    const MainSection = lazy(() => import('./_components/MainSection'));
-    const Category = lazy(() => import('./_components/Category'));
-    const CannedFood = lazy(() => import('./_components/CannedFood'));
-    const Products = lazy(() => import('./_components/Products'));
-    const Offers = lazy(() => import('./_components/Offers'));
-    const SellingProducts = lazy(() => import('./_components/SellingProducts'));
-    const OurBlog = lazy(() => import('./_components/OurBlog'));
-    const ShopApp = lazy(() => import('./_components/ShopApp'));
-    const Discover = lazy(() => import('./_components/Discover'));
-    const Footer = lazy(() => import('../../Components/Footer'));
-    const AlertOffer = lazy(() => import('./_components/AlertOffer'));
+    const { products } = useSelector((state) => state.Product)
 
-
+    if (!products) return <Loading />;
 
     return (
         <>
             <Suspense fallback={<Loading />}>
                 <Header />
+            </Suspense>
+            <Suspense fallback={<Loading />}>
                 <MainSection />
                 <Category />
                 <CannedFood />
+            </Suspense>
+            <Suspense fallback={<Loading />}>
                 <Products />
                 <Offers />
                 <SellingProducts />
+            </Suspense>
+            <Suspense fallback={<Loading />}>
                 <Discount />
                 <PopularProducts />
                 <JustArrived />
+            </Suspense>
+            <Suspense fallback={<Loading />}>
                 <OurBlog />
                 <ShopApp />
                 <Discover />
+            </Suspense>
+            <Suspense fallback={<Loading />}>
                 <Footer />
                 <AlertOffer />
             </Suspense>
